031efc0f21520b6d23658ba0c6a8e67f39d9f7dd
jvbijleveld <jeffrey@vanbijleveld.nl>
2021-07-21 21:24:05 +0200

    initial commit number 2

--- src/main/java/nl/vanbijleveld/demo/Application.java
+++ src/main/java/nl/vanbijleveld/demo/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World, I'm version 4!  And now quickly updating....";
+        return "Hello Docker World! Time for new shit!!!!!!!";
     }
 
     public static void main(String[] args) {
